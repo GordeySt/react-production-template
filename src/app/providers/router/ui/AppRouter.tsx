6f2e9a586900778c7fa353dd9df950cc0e424d1b
Hardzei <gordeystahovets@gmail.com>
2024-05-22 00:05:58 +0300

    add sidebar widget

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,5 +1,3 @@
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "shared/config/routeConfig/routeConfig";
@@ -12,7 +10,11 @@ export const AppRouter = () => {
             <Route 
               key={path}
               path={path}
-              element={element}
+              element={(
+                <div className="page-wrapper">
+                  {element}
+                </div>
+              )}
             />
           ))}
         </Routes>
