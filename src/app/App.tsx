b63565a7a739213336dc876b4797e96e6242b95c
Hardzei <gordeystahovets@gmail.com>
2024-05-21 21:29:34 +0300

    add navbar widget and applink ui kit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,17 @@
-import { Link } from 'react-router-dom'
 import './styles/index.scss'
 import { useTheme } from './providers/ThemeProvider'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 export const App = () => {  
   const { theme, toggleTheme } = useTheme();
   
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>Toggle</button>
-      <Link to={'/'}>Main Page</Link>
-      <Link to={'/about'}>About Page</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>Toggle</button>
     </div>
   )
 }
