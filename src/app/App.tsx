6aa12f1690c8331a29b46e9aaad3b2c4684ab844
Hardzei <gordeystahovets@gmail.com>
2024-05-23 16:50:24 +0300

    add i18n internalization

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,37 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
+import { Suspense } from 'react'
+import { useTranslation } from 'react-i18next'
+
+const Component = () => {
+  const { t, i18n } = useTranslation();
+
+  const toggle = () => {
+    i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
+  }
+
+  return (
+    <div>
+      <button onClick={toggle}>{t('Translate')}</button>
+      {t('Test translation')}
+    </div>
+  )
+}
 
 export const App = () => {  
   const { theme } = useTheme();
   
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Component />
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   )
 }
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,13 +1,15 @@
 import { classNames } from "shared/lib/classNames/classNames"
 import cls from "./Navbar.module.scss";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
       <div className={cls.links}>
@@ -16,13 +18,13 @@ export const Navbar = ({ className }: NavbarProps) => {
           className={cls.mainLink}
           theme={AppLinkTheme.SECONDARY}
         >
-          Main Page
+          {t('Main')}
         </AppLink>
         <AppLink
           to={'/about'} 
           theme={AppLinkTheme.SECONDARY}
         >
-          About Page
+          {t('About')}
         </AppLink>
       </div>
     </div>
