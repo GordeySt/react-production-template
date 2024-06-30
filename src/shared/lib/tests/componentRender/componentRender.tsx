d418a554e81a335d135f671acec0aac3121905ca
Hardzei Stakhavets <gordeystahovets@gmail.com>
2024-06-30 17:53:31 +0300

    fix(test): fix unit and screenshot tests.

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -6,24 +6,24 @@ import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
 
-export interface ComponentRenderOptions {
+export interface componentRenderOptions {
     route?: string;
     initialState?: DeepPartial<StateSchema>;
 }
 
-export const componentRender = (
+export function componentRender(
     component: ReactNode,
-    options: ComponentRenderOptions = {},
-) => {
+    options: componentRenderOptions = {},
+) {
     const { route = '/', initialState } = options;
 
     return render(
-        <StoreProvider initialState={initialState}>
-            <MemoryRouter initialEntries={[route]}>
+        <MemoryRouter initialEntries={[route]}>
+            <StoreProvider initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
                     {component}
                 </I18nextProvider>
-            </MemoryRouter>
-        </StoreProvider>,
+            </StoreProvider>
+        </MemoryRouter>,
     );
-};
+}
