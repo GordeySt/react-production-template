8375d836f3ada82c330a451923824b7dccc66305
Hardzei Stakhavets <gordeystahovets@gmail.com>
2024-07-20 21:04:20 +0300

    feat(config): update app config.

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,37 +1,21 @@
-/**
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
+import path from 'path';
 
-import type { Config } from 'jest';
-
-const config: Config = {
-    clearMocks: true,
-    testEnvironment: 'jsdom',
-    coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
-    moduleDirectories: ['node_modules'],
-    moduleFileExtensions: [
-        'js',
-        'mjs',
-        'cjs',
-        'jsx',
-        'ts',
-        'tsx',
-        'json',
-        'node',
-    ],
-    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
-    rootDir: '../..',
-    modulePaths: ['<rootDir>src'],
-    moduleNameMapper: {
-        '\\.s?css$': 'identity-obj-proxy',
-        '\\.svg': '<rootDir>config/jest/jestEmptyComponent.tsx',
-    },
-    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+export default {
     globals: {
         __IS_DEV__: true,
         __API__: '',
     },
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    moduleDirectories: ['node_modules'],
+    modulePaths: ['<rootDir>src'],
+    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+    rootDir: '../../',
+    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    moduleNameMapper: {
+        '\\.s?css$': 'identity-obj-proxy',
+        '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    },
 };
-
-export default config;
